ab95a56a80da742d217dbf90c2efdd5fad0d2ba9
Joel Camargo <joelc1225@gmail.com>
2017-10-07 18:06:08 -0400

    Finished exercise 2 to display 3 fragments

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -31,16 +32,37 @@ public class AndroidMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
-        // Create a new head BodyPartFragment
+        // Create all BodyPartFragments needed
         BodyPartFragment headFragment = new BodyPartFragment();
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        BodyPartFragment legsFragment = new BodyPartFragment();
 
         // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
 
-        // Add the fragment to its container using a FragmentManager and a Transaction
+        // set the list of image ids for all 3 body part fragments and set positions to second index
+        // for each
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(3);
+
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(3);
+
+        legsFragment.setImageIds(AndroidImageAssets.getLegs());
+        legsFragment.setListIndex(3);
+
+        // Add the fragments to its container using a FragmentManager and a Transactions
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
                 .add(R.id.head_container, headFragment)
+                .commit();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container, bodyFragment)
+                .commit();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.legs_container, legsFragment)
                 .commit();
 
         // TODO (5) Create and display the body and leg BodyPartFragments
